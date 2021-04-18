c4a89947abfb2e6e3d536a11f17b9d95a2fd02d6
hui <2228157440@qq.com>
2021-04-18 20:20:08 +0800

    初步开发注册功能

--- test.py
+++ test.py
@@ -9,3 +9,14 @@ GitFlow工作流练习
 # print('3、login three dev')
 
 ### login develop finished
+
+
+def register():
+    """
+    register feature developing
+    """
+
+    print('1、获取参数')
+    print('2、校验参数')
+    print('3、业务逻辑处理')
+    print('...')
